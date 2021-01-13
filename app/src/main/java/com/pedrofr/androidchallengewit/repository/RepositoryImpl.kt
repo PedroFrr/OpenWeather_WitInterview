61126fb52d9414e095ce2d226036a0b683bea401
PedroFrr <pedroandref96@gmail.com>
2021-01-13 22:26:25 +0000

    Add Hilt dependency graph and respective dependencies

--- app/src/main/java/com/pedrofr/androidchallengewit/repository/RepositoryImpl.kt
+++ app/src/main/java/com/pedrofr/androidchallengewit/repository/RepositoryImpl.kt
@@ -2,7 +2,7 @@ package com.pedrofr.androidchallengewit.repository
 
 import javax.inject.Inject
 
-class RepositoryImpl () {
+class RepositoryImpl @Inject constructor() : Repository {
 
     //TODO implement method to access/writing data
     //TODO DONT forget to write the info on the DB after obtaining it (Offline first!!)
