47c705cd2bd450a022c983758dc4ef5728f7ba5c
PedroFrr <pedroandref96@gmail.com>
2021-01-13 21:25:43 +0000

    Add two fragments for city list and city weather detail

---
+++ app/src/main/java/com/pedrofr/androidchallengewit/ui/CityWeatherDetailFragment.kt
@@ -0,0 +1,14 @@
+package com.pedrofr.androidchallengewit.ui
+
+import androidx.fragment.app.Fragment
+import com.pedrofr.androidchallengewit.R
+import com.pedrofr.androidchallengewit.databinding.FragmentCityWeatherDetailBinding
+import com.pedrofr.androidchallengewit.utils.viewBinding
+
+class CityWeatherDetailFragment : Fragment(R.layout.fragment_city_weather_detail) {
+
+    private val binding by viewBinding(FragmentCityWeatherDetailBinding::bind)
+
+
+
+}
---
+++ app/src/main/java/com/pedrofr/androidchallengewit/ui/CityWeatherListFragment.kt
@@ -0,0 +1,15 @@
+package com.pedrofr.androidchallengewit.ui
+
+import androidx.fragment.app.Fragment
+import com.pedrofr.androidchallengewit.R
+import com.pedrofr.androidchallengewit.databinding.FragmentCityListBinding
+import com.pedrofr.androidchallengewit.utils.viewBinding
+
+class CityWeatherListFragment : Fragment(R.layout.fragment_city_list) {
+
+    private val binding by viewBinding(FragmentCityListBinding::bind)
+
+
+
+
+}
